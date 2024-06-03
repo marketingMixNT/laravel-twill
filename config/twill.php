d6447831bc664a58ec8a29d8ff525094e9b9bc7c
Marek <marekgacek45@gmail.com>
2024-06-03 07:50:20 +0200

    add two block

--- config/twill.php
+++ config/twill.php
@@ -1,4 +1,33 @@
 <?php
 
 return [
+    'default_crops' => [
+        'page_cover' => [
+            'default' => [
+                [
+                    'name' => 'default',
+                    'ratio' => 16 / 9,
+                ]
+            ]
+        ]
+    ],
+    'block_editor' => [
+        'use_twill_blocks' => [],
+        'crops' => [ 
+            'highlight' => [
+                'desktop' => [
+                    [
+                        'name' => 'desktop',
+                        'ratio' => 16 / 9,
+                    ],
+                ],
+                'mobile' => [
+                    [
+                        'name' => 'mobile',
+                        'ratio' => 1,
+                    ],
+                ],
+            ],
+        ],
+    ]
 ];
