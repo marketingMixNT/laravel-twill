d7fc8ffc727e79a2c1c4b6376f8cc2743a334b5b
Marek <marekgacek45@gmail.com>
2024-06-03 10:36:53 +0200

    add localization

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -31,3 +31,50 @@ class Article extends Model
 }
 
 
+// <?php
+ 
+// namespace App\Models;
+ 
+// use A17\Twill\Models\Behaviors\HasSlug;
+// use A17\Twill\Models\Behaviors\HasTranslation;
+// use A17\Twill\Models\Model;
+// use App\Repositories\ArticleRepository;
+// use Mcamara\LaravelLocalization\Interfaces\LocalizedUrlRoutable;
+ 
+// class Article extends Model implements LocalizedUrlRoutable
+// {
+//     use HasTranslation;
+//     use HasSlug;
+ 
+//     protected $fillable = [
+//         'published',
+//         'title',
+//         'description',
+//     ];
+ 
+//     public $translatedAttributes = [
+//         'title',
+//         'description',
+//     ];
+ 
+//     public $slugAttributes = [
+//         'title',
+//     ];
+ 
+//     public function resolveRouteBinding($slug, $field = null)
+//     {
+//         $article = app(ArticleRepository::class)->forSlug($slug);
+ 
+//         abort_if(! $article, 404);
+ 
+//         return $article;
+//     }
+ 
+//     // #region routekey
+//     public function getLocalizedRouteKey($locale)
+//     {
+//         return $this->getSlug($locale);
+//     }
+ 
+//     // #endregion routekey
+// }
